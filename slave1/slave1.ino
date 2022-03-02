d4d8c3648e5fd9d7538936c6b5028e17e9bc7b19
Muhammad Juan Pradana <30497994+juanpradana@users.noreply.github.com>
2022-03-02 22:13:03 +0700

    Add files via upload

---
+++ slave1/slave1.ino
@@ -0,0 +1,23 @@
+#define Sender_Txd_pin 17
+#define Sender_Rxd_pin 16
+
+HardwareSerial Receiver(1);
+
+const int inputPin = 34;    // GPIO Pin
+const int AirValue = 3579;   //Need change for callibration for air
+const int WaterValue = 250;  //Need change for callibration for water
+float soilPercentage = 0;
+float soilMoistureValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Receiver.begin(9600, SERIAL_8N1, Sender_Rxd_pin, Sender_Txd_pin);
+}
+
+void loop() {
+  soilMoistureValue = analogRead(inputPin);
+  soilPercentage = ((((AirValue - WaterValue) - (soilMoistureValue - WaterValue)) /(AirValue - WaterValue)) * 100);
+  Serial.println("User#1: " + String(soilPercentage) + "%");
+  Receiver.print("User#1: " + String(soilPercentage));
+  delay(5000);
+}
